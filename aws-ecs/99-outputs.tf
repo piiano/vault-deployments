cc156915dc0cba921cd513714682f9cf4dac047c
Victor Bessonov <victor.a.bessonov@gmail.com>
2023-02-17 17:20:20 +0300

    Add bastion instance ID output for SSM session

--- aws-ecs/99-outputs.tf
+++ aws-ecs/99-outputs.tf
@@ -5,3 +5,7 @@ output "vault_url" {
 output "authtoken" {
   value = "Secret Manager: ${aws_secretsmanager_secret.pvault_service_admin_api_key.name} --> retrieve value"
 }
+
+output "bastion_instance_id {
+  value = one(aws_instance.bastion).id
+}
