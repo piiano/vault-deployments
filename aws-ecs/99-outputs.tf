33abc760faa715c18552ada42ac077dfa6322a7c
Victor Bessonov <victor.a.bessonov@gmail.com>
2023-02-20 12:18:24 +0300

    Goof

--- aws-ecs/99-outputs.tf
+++ aws-ecs/99-outputs.tf
@@ -6,6 +6,6 @@ output "authtoken" {
   value = "Secret Manager: ${aws_secretsmanager_secret.pvault_service_admin_api_key.name} --> retrieve value"
 }
 
-output "bastion_instance_id {
+output "bastion_instance_id" {
   value = one(aws_instance.bastion).id
 }
