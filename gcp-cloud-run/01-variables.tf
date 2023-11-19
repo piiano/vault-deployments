d69f1949cced7ef8c7893cb1b1e5790c2fa9a763
David Erel <david@piiano.com>
2023-11-19 18:31:01 +0200

    Revert "Update publish bump=patch with vault version 1.7.2"
    This reverts commit f3cb3c805cded995fdf2f6c8257c29d87239b1bb.

--- gcp-cloud-run/01-variables.tf
+++ gcp-cloud-run/01-variables.tf
@@ -110,7 +110,7 @@ variable "firewall" {
 # leave the following in a single line - publish workflow relies on it
 
 # Piiano Vault version
-variable "pvault_tag" { default = "1.7.2" }
+variable "pvault_tag" { default = "1.9.0" }
 
 variable "pvault_region" {
   description = "Vault Region. if empty fallback to default region"
